1987b55ffbea0780ef76d99a89e727c16e379d20
Adam Pingel <oss@pingel.org>
2025-02-23 14:41:53 -0500

    use rich, url_to_file, Path, and HttpUrl
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- example/rag.py
+++ example/rag.py
@@ -1,7 +1,9 @@
 
-import os
-import wget
+import asyncio
+from pathlib import Path
 from pymilvus import model
+from pydantic.networks import HttpUrl
+from proscenium.web import url_to_file
 from proscenium.vector_database import create_vector_db
 from proscenium.load import load_file
 from proscenium.chunk import documents_to_chunks_by_characters
@@ -10,32 +12,44 @@ from proscenium.vector_database import rag_prompt
 from proscenium.prompts import rag_system_prompt
 from proscenium.inference import complete_simple
 
-query = "What did Hermes say to Prometheus about giving fire to humans?"
+from rich import print
 
-data_file = "four_plays_of_aeschylus.txt"
-if not os.path.exists(data_file):
-    url = 'https://www.gutenberg.org/cache/epub/8714/pg8714.txt'
-    wget.download(url, out=data_file)
+print("[bold]Proscenium[/bold]", ":performing_arts:")
+print("[bold]The AI Alliance[/bold]")
+print()
 
+data_file = Path("four_plays_of_aeschylus.txt")
+if not data_file.exists():
+    url = HttpUrl('https://www.gutenberg.org/cache/epub/8714/pg8714.txt')
+    print("downloading", url)
+    asyncio.run(url_to_file(url, data_file))
+print("Data files to chunk:", data_file)
+
+embedding_model_id = "all-MiniLM-L6-v2"
 embedding_fn = model.dense.SentenceTransformerEmbeddingFunction(
-    model_name =  "all-MiniLM-L6-v2",
+    model_name = embedding_model_id,
     device = 'cpu' # 'cpu' or 'cuda:0'
 )
+print("Embedding model", embedding_model_id)
 
 vector_db_client, db_file_name = create_vector_db(embedding_fn) 
-print("DB file:", db_file_name)
+print("Vector db stored in temp file", db_file_name)
 
 documents = load_file(data_file)
 chunks = documents_to_chunks_by_characters(documents)
-print(f"Data file {data_file} has {len(chunks)} chunks")
+print("Data file", data_file, "has", len(chunks), "chunks")
 
 info = add_chunks_to_vector_db(vector_db_client, embedding_fn, chunks)
-print("Chunks inserted", info['insert_count'])
+print(info['insert_count'], "chunks inserted")
 
-print("Query:", query)
+query = "What did Hermes say to Prometheus about giving fire to humans?"
+
+print("USER")
+print(query)
 prompt = rag_prompt(vector_db_client, embedding_fn, query)
 
 #model_id = "ollama:llama3.2"
 model_id = "ollama:granite3.1-dense:2b"
 answer = complete_simple(model_id, rag_system_prompt, prompt)
-print("Answer:", answer)
+print("ASSISTANT")
+print(answer)
