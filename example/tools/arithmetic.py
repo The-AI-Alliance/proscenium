ad41acaa6422e0f61dc36f95306fa064c7d16ae2
Adam Pingel <oss@pingel.org>
2025-03-13 21:50:59 -0400

    ToolApplier class for Thespian actor system
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- example/tools/arithmetic.py
+++ example/tools/arithmetic.py
@@ -1,42 +1,12 @@
-
-
-from thespian.actors import Actor
-from aisuite import Client
-
-from gofannon.basic_math.addition import Addition
-from gofannon.basic_math.subtraction import Subtraction
-from gofannon.basic_math.multiplication import Multiplication
-from gofannon.basic_math.division import Division
-
-from proscenium.complete import apply_tools
-from proscenium.invoke import process_tools
-
+from rich import print
+from thespian.actors import ActorSystem
+from proscenium.display import print_header
 import example.tools.config as config
 
-class Abacus(Actor):
+print_header()
 
-    client = Client()
+tool_applier = ActorSystem().createActor(config.ToolApplier)
 
-    tool_map, tool_desc_list = process_tools([Addition, Subtraction, Multiplication, Division])
+answer = ActorSystem().ask(tool_applier, config.question, 1)
 
-    def receiveMessage(self, message, sender):
-
-        response = apply_tools(
-            system_message=config.system_message,
-            message=message,
-            tool_desc_list=self.tool_desc_list,
-            tool_map=self.tool_map,
-            client=self.client,
-            model=config.model_id
-        )
-
-        self.send(sender, response)
-
-if __name__ == '__main__':
-    from thespian.actors import ActorSystem
-    from example.tools.arithmetic import Abacus
-
-    abacus = ActorSystem().createActor(Abacus)
-
-    answer = ActorSystem().ask(abacus, config.question, 1)
-    print(answer)
+print(answer)
--- example/tools/config.py
+++ example/tools/config.py
@@ -1,7 +1,40 @@
 
+from thespian.actors import Actor
+from aisuite import Client
+
+from gofannon.basic_math.addition import Addition
+from gofannon.basic_math.subtraction import Subtraction
+from gofannon.basic_math.multiplication import Multiplication
+from gofannon.basic_math.division import Division
+
+from proscenium.complete import apply_tools
+from proscenium.invoke import process_tools
+
 model_id = "ollama:llama3.2"
 
 system_message = "Perform any referenced arithmetic."
+
+tools = [Addition, Subtraction, Multiplication, Division]
+
+class ToolApplier(Actor):
+
+    client = Client()
+
+    tool_map, tool_desc_list = process_tools(tools)
+
+    def receiveMessage(self, message, sender):
+
+        response = apply_tools(
+            system_message=system_message,
+            message=message,
+            tool_desc_list=self.tool_desc_list,
+            tool_map=self.tool_map,
+            client=self.client,
+            model=model_id
+        )
+
+        self.send(sender, response)
+
 
 question = "What is 33312-457? And what is 3+3?"
 # question = "What is your favorite color?"
