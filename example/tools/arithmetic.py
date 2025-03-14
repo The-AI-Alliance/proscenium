eebe6aa614a70f0bf36e9f186e79e38e37c1414e
Adam Pingel <oss@pingel.org>
2025-03-13 21:41:51 -0400

    mv example/arithmetic.py to example/tools, and split out a config.py
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- example/tools/arithmetic.py
+++ example/tools/arithmetic.py
@@ -3,13 +3,15 @@
 from thespian.actors import Actor
 from aisuite import Client
 
-from proscenium.complete import apply_tools
-from proscenium.invoke import process_tools
-
 from gofannon.basic_math.addition import Addition
 from gofannon.basic_math.subtraction import Subtraction
 from gofannon.basic_math.multiplication import Multiplication
 from gofannon.basic_math.division import Division
+
+from proscenium.complete import apply_tools
+from proscenium.invoke import process_tools
+
+import example.tools.config as config
 
 class Abacus(Actor):
 
@@ -17,31 +19,24 @@ class Abacus(Actor):
 
     tool_map, tool_desc_list = process_tools([Addition, Subtraction, Multiplication, Division])
 
-    system_message = "Perform any referenced arithmetic."
-
-    model = "ollama:llama3.2"
-
     def receiveMessage(self, message, sender):
 
         response = apply_tools(
-            system_message=self.system_message,
+            system_message=config.system_message,
             message=message,
             tool_desc_list=self.tool_desc_list,
             tool_map=self.tool_map,
             client=self.client,
-            model=self.model
+            model=config.model_id
         )
 
         self.send(sender, response)
 
 if __name__ == '__main__':
     from thespian.actors import ActorSystem
-    from example.arithmetic import Abacus
+    from example.tools.arithmetic import Abacus
 
     abacus = ActorSystem().createActor(Abacus)
 
-    message = "What is 33312-457? And what is 3+3?"
-    # message = "What is your favorite color?"
-
-    answer = ActorSystem().ask(abacus, message, 1)
+    answer = ActorSystem().ask(abacus, config.question, 1)
     print(answer)
---
+++ example/tools/config.py
@@ -0,0 +1,7 @@
+
+model_id = "ollama:llama3.2"
+
+system_message = "Perform any referenced arithmetic."
+
+question = "What is 33312-457? And what is 3+3?"
+# question = "What is your favorite color?"
