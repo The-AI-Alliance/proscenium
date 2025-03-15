6882b4bada808174bfb77092528a69f3d561c92f
Adam Pingel <oss@pingel.org>
2025-03-15 14:04:38 -0400

    simplify display of functions
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- proscenium/display.py
+++ proscenium/display.py
@@ -150,36 +150,41 @@ temperature: {temperature}
 
     panel = Panel(Group(
         text,
-        function_descriptions_table(tool_desc_list),
+        function_descriptions_panel(tool_desc_list),
         messages_table(messages)), title=title)
 
     return panel
 
 def parameters_table(parameters: list[dict]) -> Table:
-    table = Table(title="Parameters", show_lines=False)
-    table.add_column("name", justify="left", style="blue")
-    table.add_column("type", justify="left", style="green")
-    table.add_column("description", justify="left", style="green")
+
+    table = Table(title="Parameters", show_lines=False, box=None)
+    table.add_column("name", justify="right")
+    table.add_column("type", justify="left")
+    table.add_column("description", justify="left")
+
     for name, props in parameters['properties'].items():
         table.add_row(name, props['type'], props['description'])    
-    #"\n".join([f"{k}: {v}" for k, v in fn['parameters'].items()]
-    return table
 
-def function_descriptions_table(function_descriptions: list[dict]) -> Table:
-
-    table = Table(title="Function Descriptions", show_lines=True)
-    table.add_column("type", justify="left", style="blue")
-    table.add_column("name", justify="left", style="blue")
-    table.add_column("description", justify="left", style="green")
-    table.add_column("parameters", justify="left", style="green")
-
-    for fd in function_descriptions:
-        fn = fd['function']
-        table.add_row(
-            fd['type'],
-            fn['name'],
-            fn['description'],
-            parameters_table(fn['parameters'])
-        )
+    # TODO denote required params
 
     return table
+
+def function_description_panel(fd: dict) -> Panel:
+
+    fn = fd['function']
+
+    text = Text(f"{fd['type']} {fn['name']}: {fn['description']}\n")
+
+    pt = parameters_table(fn['parameters'])
+
+    panel = Panel(Group(text, pt))
+
+    return panel
+
+def function_descriptions_panel(function_descriptions: list[dict]) -> Panel:
+
+    sub_panels = [function_description_panel(fd) for fd in function_descriptions]
+
+    panel = Panel(Group(*sub_panels), title="Function Descriptions")
+
+    return panel
