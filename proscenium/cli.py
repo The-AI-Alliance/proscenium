77727c6db3be4ec47ce7e0454099cad6095020a8
Adam Pingel <oss@pingel.org>
2025-03-18 00:09:53 -0400

    proscenium.scripts refactor (#27)
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- proscenium/cli.py
+++ proscenium/cli.py
@@ -4,9 +4,9 @@ import logging
 from rich import print
 from proscenium.display import header
 
-from example.rag.typer_app import app as rag_app
-from example.graph_rag.typer_app import app as graph_rag_app
-from example.tools.typer_app import app as tools_app
+from proscenium.typer.abacus import app as abacus_app
+from proscenium.typer.rag import app as rag_app
+from proscenium.typer.graph_rag import app as graph_rag_app
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.WARNING)
@@ -15,7 +15,7 @@ app = typer.Typer()
 
 app.add_typer(rag_app, name="rag")
 app.add_typer(graph_rag_app, name="graph-rag")
-app.add_typer(tools_app, name="tools")
+app.add_typer(abacus_app, name="abacus")
 
 if __name__ == "__main__":
     print(header())
