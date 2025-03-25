bb95de726cb853bd1659010b657ff06a354e0cd8
Adam Pingel <oss@pingel.org>
2025-03-24 20:15:08 -0400

    rename rag to literature, and graph-rag to legal
    Signed-off-by: Adam Pingel <oss@pingel.org>

---
+++ proscenium/typer/legal.py
@@ -0,0 +1,114 @@
+
+import typer
+from rich import print
+from rich.panel import Panel
+
+from proscenium.verbs.vector_database import create_vector_db
+from proscenium.verbs.vector_database import embedding_function
+from proscenium.verbs.vector_database import vector_db
+from proscenium.verbs.know import knowledge_graph_client
+from proscenium.verbs.vector_database import collection_name
+
+from proscenium.scripts.graph_rag import extract_entities, \
+    load_entity_graph, show_entity_graph, load_entity_resolver, \
+    answer_question
+
+from proscenium.typer import legal_config
+
+app = typer.Typer()
+
+@app.command()
+def extract():
+
+    extract_entities(
+        legal_config.retrieve_documents,
+        legal_config.doc_as_rich,
+        legal_config.entity_csv_file,
+        legal_config.doc_direct_triples,
+        legal_config.default_chunk_extraction_model_id,
+        legal_config.get_triples_from_chunk)
+
+@app.command()
+def load_graph():
+
+    driver = knowledge_graph_client(
+        legal_config.neo4j_uri,
+        legal_config.neo4j_username,
+        legal_config.neo4j_password)
+
+    load_entity_graph(
+        driver,
+        legal_config.entity_csv_file,
+        legal_config.add_triple)
+
+    driver.close()
+
+@app.command()
+def show_graph():
+
+    driver = knowledge_graph_client(
+        legal_config.neo4j_uri,
+        legal_config.neo4j_username,
+        legal_config.neo4j_password)
+
+    show_entity_graph(driver)
+
+    driver.close()
+
+@app.command()
+def load_resolver():
+    embedding_fn = embedding_function(legal_config.embedding_model_id)
+    print("Embedding model", legal_config.embedding_model_id)
+
+    vector_db_client = create_vector_db(legal_config.milvus_uri, embedding_fn, overwrite=True)
+    print("Vector db stored at", legal_config.milvus_uri)
+
+    driver = knowledge_graph_client(
+        legal_config.neo4j_uri,
+        legal_config.neo4j_username,
+        legal_config.neo4j_password)
+
+    load_entity_resolver(driver, vector_db_client, embedding_fn, collection_name)
+
+    driver.close()
+    vector_db_client.close()
+
+
+@app.command()
+def ask():
+
+    embedding_fn = embedding_function(legal_config.embedding_model_id)
+    vector_db_client = vector_db(legal_config.milvus_uri)
+    print("Connected to vector db stored at", legal_config.milvus_uri, "with embedding model", legal_config.embedding_model_id)
+    print("\n")
+
+    driver = knowledge_graph_client(
+        legal_config.neo4j_uri,
+        legal_config.neo4j_username,
+        legal_config.neo4j_password)
+
+    question = legal_config.get_user_question()
+
+    answer = answer_question(
+        question,
+        legal_config.retrieve_document,
+        legal_config.default_generation_model_id,
+        legal_config.system_prompt,
+        legal_config.graphrag_prompt_template,
+        driver,
+        vector_db_client,
+        embedding_fn,
+        legal_config.query_extraction_template,
+        legal_config.query_extraction_model_id,
+        legal_config.query_extraction_data_model,
+        legal_config.get_triples_from_query_extract,
+        legal_config.matching_objects_query,
+        )
+
+    if answer:
+        print(Panel(answer, title="Answer"))
+    else:
+        print("No answer")
+
+    driver.close()
+    vector_db_client.close()
---
+++ proscenium/typer/literature.py
@@ -0,0 +1,49 @@
+import typer
+import os
+from rich import print
+from rich.panel import Panel
+import asyncio
+
+from proscenium.verbs.read import url_to_file
+from proscenium.verbs.vector_database import embedding_function
+from proscenium.verbs.vector_database import create_vector_db
+from proscenium.verbs.vector_database import vector_db
+
+from proscenium.scripts.rag import answer_question, build_vector_db as bvd
+from proscenium.typer import literature_config
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+app = typer.Typer()
+
+@app.command()
+def build_vector_db():
+
+    asyncio.run(url_to_file(literature_config.url, literature_config.data_file))
+    print("Data file to chunk:", literature_config.data_file)
+
+    embedding_fn = embedding_function(literature_config.embedding_model_id)
+    print("Embedding model", literature_config.embedding_model_id)
+
+    vector_db_client = create_vector_db(literature_config.milvus_uri, embedding_fn, overwrite=True)
+    print("Vector db at uri", literature_config.milvus_uri)
+
+    bvd(literature_config.data_file, vector_db_client, embedding_fn)
+
+    vector_db_client.close()
+
+
+@app.command()
+def ask():
+
+    query = literature_config.get_user_question()
+
+    embedding_fn = embedding_function(literature_config.embedding_model_id)
+    print("Embedding model:", literature_config.embedding_model_id)
+
+    vector_db_client = vector_db(literature_config.milvus_uri)
+    print("Vector db at uri", literature_config.milvus_uri)
+
+    answer = answer_question(query, literature_config.model_id, vector_db_client, embedding_fn)
+
+    print(Panel(answer, title="Assistant"))
