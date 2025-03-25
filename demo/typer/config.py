eaea7fe1b6ea5ec679932a281559a258a2d820c3
Adam Pingel <oss@pingel.org>
2025-03-25 00:54:02 -0400

    rm old versions of config files
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- demo/typer/config.py
+++
@@ -1,9 +0,0 @@
-
-
-#default_model_id = "together:meta-llama/Llama-3-8b-chat-hf"
-default_model_id = "together:meta-llama/Llama-3-70b-chat-hf"
-#default_model_id = "together:meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
-
-# default_model_id = "ollama:llama3.2"
-# default_model_id = "ollama:granite3.2"
-# default_model_id = "openai:gpt-4o"
