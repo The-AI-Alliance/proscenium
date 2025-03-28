de38d46255950869c8da3a9ebff2139ea5394d81
Adam Pingel <oss@pingel.org>
2025-03-28 13:40:20 -0400

    a few simple integration test scripts.  not yet automated.
    Signed-off-by: Adam Pingel <oss@pingel.org>

---
+++ integration/abacus.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "Abacus Integration Test"
+
+echo "What is 1 + 1?" | python -m demo.cli abacus ask
+
---
+++ integration/all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "All Integration Tests"
+
+bash abacus.sh
+
+bash literature.sh
+
+bash legal.sh
