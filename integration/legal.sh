ea5e55b841487f9b971e97902dcbb951bac2a992
Adam Pingel <oss@pingel.org>
2025-04-02 14:02:45 -0400

    less verbose
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- integration/legal.sh
+++ integration/legal.sh
@@ -2,20 +2,28 @@
 
 echo "Legal Integration Test"
 
-set +x
-
 JSONL_FILE=test-enrichments.jsonl
+
+echo "Enriching documents"
 
 python -m demo.cli legal enrich \
   --docs-per-dataset=2 \
   --output=$JSONL_FILE
 
+echo "Loading documents into Knowledge Graph"
+
 python -m demo.cli legal load-graph \
   --input=$JSONL_FILE
 
+echo "Displaying Knowledge Graph"
+
 python -m demo.cli legal show-graph
 
+echo "Loading fields into Milvus"
+
 python -m demo.cli legal load-resolver
+
+echo "Answer default legal question"
 
 echo "" | python -m demo.cli legal ask
 
