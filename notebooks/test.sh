ef9ca9e36a3ba456740abebb3aea1ad4a51eea8b
Adam Pingel <oss@pingel.org>
2025-04-17 23:58:31 -0400

    clean up side effects of notebook execution
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- notebooks/test.sh
+++ notebooks/test.sh
@@ -5,6 +5,12 @@ PYTHONPATH=..
 # pip install jupyter
 
 for NOTEBOOK in $(find . -name "*.ipynb"); do
+
     echo "Running $NOTEBOOK"
+
     PYTHONPATH=$PYTHONPATH jupyter nbconvert --to markdown --execute --stdout "$NOTEBOOK" > "${NOTEBOOK%.ipynb}.md"
+
+    # Clean up side effects
+    rm -rf proscenium
+    rm -f "${NOTEBOOK%.ipynb}.md"
 done
