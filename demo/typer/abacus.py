5ef65cfba879a9e58a525949544b3fbae2dc8827
Adam Pingel <oss@pingel.org>
2025-04-27 16:40:10 -0400

    remove thespian / actor demo
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- demo/typer/abacus.py
+++ demo/typer/abacus.py
@@ -2,11 +2,10 @@ from rich import print
 from rich.panel import Panel
 from rich.prompt import Prompt
 import typer
-from thespian.actors import ActorSystem
 
 from demo.config import default_model_id
 
-from proscenium.scripts.tools import apply_tools, tool_applier_actor_class
+from proscenium.scripts.tools import apply_tools
 
 model_id = default_model_id
 
@@ -31,26 +30,5 @@ def ask():
         tool_map=domain.tool_map,
         rich_output=True,
     )
-
-    print(Panel(answer, title="Answer"))
-
-
-@app.command(
-    help="Ask a natural langauge arithmetic question. (Implemented with actors.)"
-)
-def ask_actor():
-
-    abacus_actor_class = tool_applier_actor_class(
-        tools=domain.tools,
-        system_message=domain.system_message,
-        model_id=model_id,
-        rich_output=True,
-    )
-
-    tool_applier = ActorSystem().createActor(abacus_actor_class)
-
-    question = "What is 33312-457? And what is 3+3?"
-
-    answer = ActorSystem().ask(tool_applier, question, 1)
 
     print(Panel(answer, title="Answer"))
