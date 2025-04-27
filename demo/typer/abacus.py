452243e7b7c888e768ba92ee80dc934ca919be5c
Adam Pingel <oss@pingel.org>
2025-04-27 16:45:49 -0400

    abacus typer app should use make_handler
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- demo/typer/abacus.py
+++ demo/typer/abacus.py
@@ -3,12 +3,6 @@ from rich.panel import Panel
 from rich.prompt import Prompt
 import typer
 
-from demo.config import default_model_id
-
-from proscenium.scripts.tools import apply_tools
-
-model_id = default_model_id
-
 import demo.domains.abacus as domain
 
 app = typer.Typer(help="""Arithmetic question answering.""")
@@ -17,18 +11,12 @@ app = typer.Typer(help="""Arithmetic question answering.""")
 @app.command(help="Ask a natural langauge arithmetic question.")
 def ask():
 
+    handler = domain.make_handler(verbose=True)
+
     question = Prompt.ask(
         f"What is your arithmetic question?",
         default="What is 33312-457? And what is 3+3?",
     )
 
-    answer = apply_tools(
-        model_id=model_id,
-        system_message=domain.system_message,
-        message=question,
-        tool_desc_list=domain.tool_desc_list,
-        tool_map=domain.tool_map,
-        rich_output=True,
-    )
-
-    print(Panel(answer, title="Answer"))
+    for message in handler(question):
+        print(Panel(message, title="Answer"))
