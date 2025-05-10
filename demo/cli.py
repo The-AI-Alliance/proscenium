181d045ecc92740cda4c5fe0ea3d1f3aa47954a4
Adam Pingel <oss@pingel.org>
2025-05-09 22:18:31 -0400

    rm legal
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- demo/cli.py
+++ demo/cli.py
@@ -6,7 +6,6 @@ from proscenium.verbs.display import header
 
 from demo.typer.abacus import app as abacus_app
 from demo.typer.literature import app as literature_app
-from demo.typer.legal import app as legal_app
 
 log = logging.getLogger(__name__)
 
@@ -16,7 +15,6 @@ logging.getLogger("demo").setLevel(logging.WARNING)
 app = typer.Typer(help="CLI Demo of Proscenium")
 
 app.add_typer(literature_app, name="literature")
-app.add_typer(legal_app, name="legal")
 app.add_typer(abacus_app, name="abacus")
 
 if __name__ == "__main__":
