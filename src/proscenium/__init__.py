d9b64818571b72ad40c4148e3281ec10c55d1828
Adam Pingel <oss@pingel.org>
2025-06-04 23:02:22 -0400

    remove proscenium.verbs.display
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- src/proscenium/__init__.py
+++ src/proscenium/__init__.py
@@ -1,3 +1,13 @@
 import logging
 
+from rich.text import Text
+
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+def header() -> Text:
+    text = Text()
+    text.append("Proscenium 🎭\n", style="bold")
+    text.append("https://the-ai-alliance.github.io/proscenium/\n")
+    # TODO version, timestamp, ...
+    return text
--- src/proscenium/verbs/remember.py
+++ src/proscenium/verbs/remember.py
@@ -1,5 +1,7 @@
 import logging
 
+from rich.table import Table
+
 log = logging.getLogger(__name__)
 
 
@@ -11,3 +13,37 @@ def format_chat_history(chat_history) -> str:
             for msg in chat_history
         ]
     )
+
+
+def messages_table(messages: list) -> Table:
+
+    table = Table(title="Messages in Chat Context", show_lines=True)
+    table.add_column("Role", justify="left")
+    table.add_column("Content", justify="left")
+    for message in messages:
+        if type(message) is dict:
+            role = message["role"]
+            content = ""
+            if role == "tool":
+                content = f"""tool call id = {message['tool_call_id']}
+fn name = {message['name']}
+result = {message['content']}"""
+            elif role == "assistant":
+                content = f"""{str(message)}"""
+            else:
+                content = message["content"]
+            table.add_row(role, content)
+        else:
+            role = message.role
+            content = ""
+            if role == "tool":
+                content = f"""tool call id = {message.tool_call_id}
+fn name = {message.name}
+result = {message['content']}"""
+            elif role == "assistant":
+                content = f"""{str(message)}"""
+            else:
+                content = message.content
+            table.add_row(role, content)
+
+    return table
